ce97707ea1fc006d9a24aa51fa868e673c0668d8
Alex C. Rocha <rocha.amc@gmail.com>
2021-03-13 11:23:52 -0800

    Add randomize function

---
+++ main.js
@@ -0,0 +1,12 @@
+// message generator program.
+// runs a program > should get a new
+// randomized output
+// three different pieces of data.
+
+const randomize = () => {
+  return Math.floor(Math.random() * 3);
+};
+
+let partOne = [];
+let partTwo = [];
+let partThree = [];
