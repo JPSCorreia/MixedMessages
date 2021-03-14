704c2e58ff95d37330e606166021a4d9e1ece70b
Alex C. Rocha <rocha.amc@gmail.com>
2021-03-13 19:35:58 -0800

    Add messageMixer

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-<<<<<<< HEAD
 // sync test
-=======
 // message generator program.
 // runs a program > should get a new
 // randomized output
 // three different pieces of data.
 
-const randomize = () => {
-  return Math.floor(Math.random() * 3);
+const randomize = (arraySize) => {
+  return Math.floor(Math.random() * arraySize);
 };
 
-let partOne = [];
-let partTwo = [];
-let partThree = [];
->>>>>>> ce97707ea1fc006d9a24aa51fa868e673c0668d8
+let partOne = ["O Pedro", "A Maria", "O Joaquim"];
+let partTwo = [
+  "caiu no buraco",
+  "comeu uma banana podre",
+  "foi dormir mal-cheiroso",
+];
+let partThree = [
+  "pela tarde",
+  "durante o jogo do Benfica",
+  "sem os pais saberem",
+];
+
+const messageMixer = () => {
+  const messageOne = partOne[randomize(partOne.length)];
+  const messageTwo = partTwo[randomize(partTwo.length)];
+  const messageThree = partThree[randomize(partThree.length)];
+  return `${messageOne} ${messageTwo} ${messageThree}.`;
+};
+
+console.log(messageMixer());
+
